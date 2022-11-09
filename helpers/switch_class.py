719a5c39488295f90e05af973e709332212e8d00
Ivan Shires <ipshires1@catamount.wcu.edu>
2022-11-08 21:39:37 -0500

    added logic+commandset

--- helpers/switch_class.py
+++ helpers/switch_class.py
@@ -1,11 +1,18 @@
 from netmiko import ConnectHandler
 import os
+from datetime import datetime
 
 ## Switch Credentials
-switch_user = os.environ['network_switch_usr'])
-switch_password = os.environ['network_switch_pwd'])
+switch_user = os.environ['network_switch_usr']
+switch_password = os.environ['network_switch_pwd']
 
-## Versions that do NOT support 'no vstack'
+## Logging Directory
+logging_directory = "/opt/service-SmartInstall-vuln-fixer/"
+
+def get_time():
+    now = datetime.now()
+    dt_string = now.strftime("%m-%d-%Y %H-%M")
+    return dt_string
 
 class switch_connect:
     def __init__(self,switch_ipaddr):
@@ -15,7 +22,7 @@ class switch_connect:
     def get_information(self):
         command = "show version"
         device = {
-            "device_type": "cisco_ios_telnet",
+            "device_type": "cisco_ios",
             "host": str(self.switch_ipaddr),
             "username": str(switch_user),
             "password": str(switch_password),
@@ -23,6 +30,93 @@ class switch_connect:
         }
         with ConnectHandler(**device) as net_connect:
             self.output = net_connect.send_command(command,use_textfsm=True)
-        
+        return self.output
+
+    def smartinstall_vstack(self):
+        commands = [
+            'no vstack',
+            ]
+
+        device = {
+            "device_type": "cisco_ios",
+            "host": str(self.switch_ipaddr),
+            "username": str(switch_user),
+            "password": str(switch_password),
+            "session_log": logging_directory + str(self.switch_ipaddr) + "-" + str(get_time()) + ".txt",
+            "timeout": 30,
+        }
+        with ConnectHandler(**device) as net_connect:
+            self.output = net_connect.send_config_set(commands)
+            self.output += net_connect.save_config()
+
+    def create_smartinstall_acl(self):
+        commands = [
+            'access-list 100 deny tcp any any eq 4786',
+            'access-list 100 deny udp any any eq 4786',
+            'access-list 100 permit ip any any',
+            'interface GigabitEthernet1',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet2',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/1/1', ## 3750x/3650/3850 with uplink module.. maybe 3750E with SFP usage
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/1/2', ## 3750x/3650/3850 with uplink module.. maybe 3750E with SFP usage
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/1', ## 3560x with uplink module 
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/2', ## 3560x with uplink module
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 0/47',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 0/48',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/0/47', ## 3750x/3750e
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface GigabitEthernet 1/0/48',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 1/1', ## 3560 using SFP ports
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 1/2', ## 3560 using SFP ports
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 0/47', ## 3560
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 0/48', ## 3560
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 1/0/47',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface FastEthernet 1/0/48',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            'interface vlan 1',
+            'ip access-group 100 in',
+            'ip access-group 100 out',
+            ]
+        device = {
+            "device_type": "cisco_ios",
+            "host": str(self.switch_ipaddr),
+            "username": str(switch_user),
+            "password": str(switch_password),
+            "session_log": logging_directory + str(self.switch_ipaddr) + "-" + str(get_time()) + ".txt",
+            "timeout": 30,
+        }
+        with ConnectHandler(**device) as net_connect:
+            self.output = net_connect.send_config_set(commands)
+            self.output += net_connect.save_config()
+
     def get_version(self):
         return self.output[0]['version']
--- main.py
+++ main.py
@@ -11,13 +11,14 @@ from helpers.switch_class import *
 ## SmartInstall Port
 network_port = 4786
 
-## Logging Directory
-logging_directory = "/opt/service-SmartInstall-vuln-fixer"
-
 ## Switch Credentials
-#switch_user = os.environ['network_switch_usr']
-#switch_password = os.environ['network_switch_pwd']
+switch_user = os.environ['network_switch_usr']
+switch_password = os.environ['network_switch_pwd']
 
+## Versions that do NOT support "no vstack"
+bad_versions_file = open("smartinstall_bad_versions.txt","r")
+bad_versions = bad_versions_file.readlines()
+bad_versions = [item.rstrip() for item in bad_versions]
 
 def check_network_port(target,port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -34,6 +35,15 @@ vulnerability_status = check_network_port(host,network_port)
 
 if (vulnerability_status):
     print(host,"is vulnerable!")
-
+    switch = switch_connect(host)
+    switch_version = switch.get_version()
+    if (any(bad_version in switch_version for bad_version in bad_versions)):
+        print("Requires ACL")
+        switch = switch_connect(host)
+        switch.create_smartinstall_acl()
+    else:
+        print("Running 'no vstack'")
+        switch = switch_connect(host)
+        switch.smartinstall_vstack()
 else:
     print(host,"is OK")
